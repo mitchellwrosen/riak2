5ffaf9d7628591f8be88f1c750ec91b55c68052f
Mitchell Rosen <mitchellwrosen@gmail.com>
2018-08-09 20:56:28 -0500

    Use streaming byte string for server messages

--- src/Riak.hs
+++ src/Riak.hs
@@ -5,16 +5,26 @@ module Riak
   , withHandle
   ) where
 
+import Control.Monad
 import Control.Monad.IO.Class  (MonadIO, liftIO)
 import Control.Monad.IO.Unlift
+import Data.ByteString         (ByteString)
+import Data.IORef
+import Data.Void
 import Network.Socket          (AddrInfo(..), HostName, PortNumber, Socket,
                                 SocketType(Stream), defaultHints, getAddrInfo)
 import UnliftIO.Exception      (bracket)
 
-import qualified Network.Socket as Network
+import qualified Data.ByteString           as ByteString
+import qualified Data.ByteString.Streaming as Q
+import qualified Network.Socket            as Network hiding (recv)
+import qualified Network.Socket.ByteString as Network (recv)
 
+-- | A non-thread-safe handle to Riak.
 data Handle
-  = Handle !Socket
+  = Handle
+      !Socket                         -- Server socket
+      !(IORef (Q.ByteString IO Void)) -- Infinite input from server
 
 withHandle
   :: MonadUnliftIO m
@@ -37,8 +47,22 @@ connect host port = liftIO $ do
   socket :: Socket <-
     Network.socket (addrFamily info) (addrSocketType info) (addrProtocol info)
 
-  pure (Handle socket)
+  let
+    source :: Q.ByteString IO Void
+    source =
+      forever $ do
+        bytes :: ByteString <-
+          liftIO (Network.recv socket 4096)
+        if ByteString.null bytes
+          -- TODO Properly handle Riak closing connection
+          then error "Riak closed the connection"
+          else Q.chunk bytes
+
+  sourceRef :: IORef (Q.ByteString IO Void) <-
+    newIORef source
+
+  pure (Handle socket sourceRef)
 
 disconnect :: MonadIO m => Handle -> m ()
-disconnect (Handle socket) =
+disconnect (Handle socket _) =
   liftIO (Network.close socket)
