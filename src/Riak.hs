b198505b86ef61e4c1a7c6d6731ff9b87a057ac3
Mitchell Rosen <mitchellwrosen@gmail.com>
2018-08-09 21:14:46 -0500

    Add send function

--- src/Riak.hs
+++ src/Riak.hs
@@ -9,22 +9,33 @@ import Control.Monad
 import Control.Monad.IO.Class  (MonadIO, liftIO)
 import Control.Monad.IO.Unlift
 import Data.ByteString         (ByteString)
+import Data.ByteString.Builder (Builder)
 import Data.IORef
 import Data.Void
+import Data.Word
 import Network.Socket          (AddrInfo(..), HostName, PortNumber, Socket,
                                 SocketType(Stream), defaultHints, getAddrInfo)
 import UnliftIO.Exception      (bracket)
 
 import qualified Data.ByteString           as ByteString
+import qualified Data.ByteString.Builder   as Builder
+import qualified Data.ByteString.Lazy      as Lazy (ByteString)
 import qualified Data.ByteString.Streaming as Q
-import qualified Network.Socket            as Network hiding (recv)
+import qualified Network.Socket            as Network hiding (recv, sendAll)
 import qualified Network.Socket.ByteString as Network (recv)
+import qualified Network.Socket.ByteString.Lazy as Network (sendAll)
 
 -- | A non-thread-safe handle to Riak.
 data Handle
   = Handle
       !Socket                         -- Server socket
       !(IORef (Q.ByteString IO Void)) -- Infinite input from server
+      (Lazy.ByteString -> IO ())
+
+data Message
+  = Message
+      !Word8      -- Message code
+      !ByteString -- Message payload
 
 withHandle
   :: MonadUnliftIO m
@@ -61,8 +72,25 @@ connect host port = liftIO $ do
   sourceRef :: IORef (Q.ByteString IO Void) <-
     newIORef source
 
-  pure (Handle socket sourceRef)
+  let
+    sink :: Lazy.ByteString -> IO ()
+    sink =
+      Network.sendAll socket
+
+  pure (Handle socket sourceRef sink)
 
 disconnect :: MonadIO m => Handle -> m ()
-disconnect (Handle socket _) =
+disconnect (Handle socket _ _) =
   liftIO (Network.close socket)
+
+-- | Send a 'Message' on a 'Handle'.
+send :: Handle -> Message -> IO ()
+send (Handle _ _ sink) (Message code bytes) =
+  sink payload
+ where
+  payload :: Lazy.ByteString
+  payload =
+    Builder.toLazyByteString
+      (Builder.int32BE (fromIntegral (ByteString.length bytes + 1))
+        <> Builder.word8 code
+        <> Builder.byteString bytes)
