875325e1a9be90c302ad6ed3072a34557b0c8e5c
Mitchell Rosen <mitchellwrosen@gmail.com>
2019-01-23 16:37:17 -0500

    API refactoring

--- riak2/src/Riak/Client.hs
+++ riak2/src/Riak/Client.hs
@@ -2,26 +2,6 @@ module Riak.Client
   ( -- * Client
     Client
   , new
-    -- * Object operations
-    -- ** Get object
-  , get
-  , getHead
-  , getIfModified
-  , getHeadIfModified
-  , IfModified(..)
-    -- ** Put object
-  , put
-  , putGet
-  , putGetHead
-    -- ** Delete object
-  , delete
-    -- * Counter operations
-    -- ** Get counter
-  , getCounter
-    -- ** Update counter
-  , updateCounter
   ) where
 
 import Riak.Internal.Client
-import Riak.Internal.Client.Counter
-import Riak.Internal.Client.Object
