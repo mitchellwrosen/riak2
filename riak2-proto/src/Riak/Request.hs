21273cb70124892cc2f752511229ae6b0b09042e
Mitchell Rosen <mitchellwrosen@gmail.com>
2019-01-26 13:35:38 -0500

    optimize request serialization

--- riak2-proto/src/Riak/Request.hs
+++ riak2-proto/src/Riak/Request.hs
@@ -5,14 +5,16 @@ module Riak.Request
 
 import Riak.Proto
 
-import Data.ByteString (ByteString)
-import Data.Word       (Word8)
+import Data.Bits          (unsafeShiftR)
+import Data.ByteString    (ByteString)
+import Data.Word          (Word32, Word8)
+import Foreign.ForeignPtr (withForeignPtr)
+import Foreign.Ptr        (Ptr, plusPtr)
+import Foreign.Storable   (poke)
 
-import qualified Data.Attoparsec.ByteString as Atto
-import qualified Data.ByteString            as ByteString
-import qualified Data.ByteString.Builder    as Builder
-import qualified Data.ByteString.Lazy       as Lazy (ByteString)
-import qualified Data.ProtoLens             as Proto
+import qualified Data.ByteString          as ByteString
+import qualified Data.ByteString.Internal as ByteString
+import qualified Data.ProtoLens           as Proto
 
 
 data Request
@@ -53,36 +55,43 @@ instance Show Request where
     RequestStreamKeys              request -> show request
     RequestUpdateCrdt              request -> show request
 
-encode :: Request -> Lazy.ByteString
+encode :: Request -> ByteString
 encode = \case
-  RequestDelete                  request -> go 13 request
-  RequestGet                     request -> go  9 request
-  RequestGetBucketProperties     request -> go 19 request
-  RequestGetBucketTypeProperties request -> go 31 request
-  RequestGetCrdt                 request -> go 80 request
-  RequestGetServerInfo           request -> go  7 request
-  RequestIndex                   request -> go 25 request
-  RequestMapReduce               request -> go 23 request
-  RequestPing                    request -> go  1 request
-  RequestPut                     request -> go 11 request
-  RequestResetBucketProperties   request -> go 29 request
-  RequestSetBucketProperties     request -> go 21 request
-  RequestSetBucketTypeProperties request -> go 31 request
-  RequestStreamBuckets           request -> go 15 request
-  RequestStreamKeys              request -> go 17 request
-  RequestUpdateCrdt              request -> go 82 request
+  RequestDelete                  request -> encode' 13 (Proto.encodeMessage request)
+  RequestGet                     request -> encode'  9 (Proto.encodeMessage request)
+  RequestGetBucketProperties     request -> encode' 19 (Proto.encodeMessage request)
+  RequestGetBucketTypeProperties request -> encode' 31 (Proto.encodeMessage request)
+  RequestGetCrdt                 request -> encode' 80 (Proto.encodeMessage request)
+  RequestGetServerInfo           request -> encode'  7 (Proto.encodeMessage request)
+  RequestIndex                   request -> encode' 25 (Proto.encodeMessage request)
+  RequestMapReduce               request -> encode' 23 (Proto.encodeMessage request)
+  RequestPing                    request -> encode'  1 (Proto.encodeMessage request)
+  RequestPut                     request -> encode' 11 (Proto.encodeMessage request)
+  RequestResetBucketProperties   request -> encode' 29 (Proto.encodeMessage request)
+  RequestSetBucketProperties     request -> encode' 21 (Proto.encodeMessage request)
+  RequestSetBucketTypeProperties request -> encode' 31 (Proto.encodeMessage request)
+  RequestStreamBuckets           request -> encode' 15 (Proto.encodeMessage request)
+  RequestStreamKeys              request -> encode' 17 (Proto.encodeMessage request)
+  RequestUpdateCrdt              request -> encode' 82 (Proto.encodeMessage request)
 
-  where
-    go :: Proto.Message a => Word8 -> a -> Lazy.ByteString
-    go code request =
-      Builder.toLazyByteString
-        (Builder.int32BE (fromIntegral (ByteString.length bytes + 1))
-          <> Builder.word8 code
-          <> Builder.byteString bytes)
-      where
-        bytes :: ByteString
-        bytes =
-          Proto.encodeMessage request
+encode' :: Word8 -> ByteString -> ByteString
+encode' code request =
+  ByteString.unsafeCreate (5 + ByteString.length request) $ \ptr -> do
+    pokeWord32BE ptr (fromIntegral (1 + ByteString.length request))
+    poke (ptr `plusPtr` 4) code
+    pokeByteString (ptr `plusPtr` 5) request
+
+pokeByteString :: Ptr Word8 -> ByteString -> IO ()
+pokeByteString dst (ByteString.PS srcf offset len) =
+  withForeignPtr srcf $ \src ->
+    ByteString.memcpy dst (src `plusPtr` offset) len
+
+pokeWord32BE :: Ptr Word8 -> Word32 -> IO ()
+pokeWord32BE ptr word = do
+  poke  ptr              (fromIntegral (unsafeShiftR word 24) :: Word8)
+  poke (ptr `plusPtr` 1) (fromIntegral (unsafeShiftR word 16) :: Word8)
+  poke (ptr `plusPtr` 2) (fromIntegral (unsafeShiftR word  8) :: Word8)
+  poke (ptr `plusPtr` 3) (fromIntegral (             word   ) :: Word8)
 
 -- instance Request RpbSearchQueryReq         where code = 27
 -- instance Request RpbYokozunaIndexDeleteReq where code = 57
