cf8b283e6b69cde88b565f98bf1eb5febbff33e2
Mitchell Rosen <mitchellwrosen@gmail.com>
2019-02-10 00:20:02 -0500

    Reimplement exclusive socket test suite

--- riak2-handle-impl-exclusive/test/Main.hs
+++ riak2-handle-impl-exclusive/test/Main.hs
@@ -1,216 +1,104 @@
 module Main where
 
-main = pure ()
+import Riak.Handle.Impl.Exclusive
+import Riak.Request               (Request(..))
 
--- import Riak.Request  (Request(..))
--- import Riak.Response (Response(..))
--- import Riak.Socket   (Socket(..))
+import qualified Riak.Proto as Proto
 
--- import qualified Riak.Handle.Impl.Exclusive as Handle
--- import qualified Riak.Proto                 as Proto
--- import qualified Riak.Socket                as Socket
+import Control.Concurrent
+import Control.Exception  (throwIO)
+import Control.Monad
+import Data.IORef
+import Net.IPv4           (ipv4)
+import System.Exit
 
--- import Control.Concurrent
--- import Control.Concurrent.STM
--- import Control.Monad
--- import System.Random
+data Action
+  = Send
+  | Receive
+  deriving (Show)
 
--- import qualified Data.ProtoLens as Proto
--- import qualified Network.Socket as Network
+main :: IO ()
+main = do
+  actionsRef :: IORef [Action] <-
+    newIORef []
 
--- main :: IO ()
--- main = do
---   pure () -- socketmain
+  let
+    config :: Config
+    config =
+      Config
+        { endpoint =
+            Endpoint
+              { address = ipv4 127 0 0 1
+              , port = 8087
+              }
+        , handlers =
+            EventHandlers
+              { onSend =
+                  \_ ->
+                    atomicModifyIORef
+                      actionsRef
+                      (\actions -> (Send : actions, ()))
+              , onReceive =
+                  \_ ->
+                    atomicModifyIORef
+                      actionsRef
+                      (\actions -> (Receive : actions, ()))
+              }
+        }
 
--- socketmain :: IO ()
--- socketmain = do
---   client <- mockServer
+  let threads = 1000
 
---   let
---     config :: Handle.Config
---     config =
---       Handle.Config
---         { Handle.socket = client
---         , Handle.handlers = mempty
---         }
+  doneVar <- newEmptyMVar
+  cherryTree <- newCherryTree threads
 
---   Handle.withHandle config $ \iface -> do
+  result <-
+    withHandle config $ \handle -> do
+      replicateM_ threads $ forkIO $ do
+        chopCherryTree cherryTree
 
---     let workers = 10
---     let requests = 1000
+        _ <- exchange handle (RequestPing Proto.defMessage)
 
---     readyVar <- newTVarIO False
---     doneVar <- newEmptyMVar
+        putMVar doneVar ()
 
---     replicateM_ workers . forkIO $ do
---       atomically (readTVar readyVar >>= flip unless retry)
+      replicateM_ threads (takeMVar doneVar)
 
---       replicateM_ requests $ do
---         request <-
---           randomOneOf
---             [ RequestPing Proto.defMessage
---             , RequestGet Proto.defMessage
---             , RequestPut Proto.defMessage
---             , RequestDelete Proto.defMessage
---             ]
+  actions <- readIORef actionsRef
+  shouldBeSendRecv (reverse actions)
 
---         response <- Handle.exchange iface request
+  case result of
+    Left errno ->
+      exitWith (ExitFailure (fromIntegral errno))
 
---         unless (expectedResponse request response) $ do
---           putStrLn "ERROR"
---           putStrLn (">>> " ++ show request)
---           putStrLn ("<<< " ++ show response)
+    Right () ->
+      pure ()
 
---       putMVar doneVar ()
+shouldBeSendRecv :: [Action] -> IO ()
+shouldBeSendRecv = \case
+  [] -> pure ()
+  Send : xs -> shouldBeRecvSend xs
+  Receive : _ -> throwIO (userError "expected Send")
 
---     atomically (writeTVar readyVar True)
+shouldBeRecvSend :: [Action] -> IO ()
+shouldBeRecvSend = \case
+  [] -> throwIO (userError "missing Receive")
+  Receive : xs -> shouldBeSendRecv xs
+  Send : _ -> throwIO (userError "expected Receive")
 
---     replicateM_ workers (takeMVar doneVar)
+type CherryTree
+  = (Int, MVar (Int, MVar ()))
 
--- socketPair :: IO (Socket, Socket)
--- socketPair = do
---   (client, server) <-
---     Network.socketPair
---       Network.AF_UNIX
---       Network.Stream
---       Network.defaultProtocol
+newCherryTree :: Int -> IO CherryTree
+newCherryTree n = do
+  doneVar <- newEmptyMVar
+  chopVar <- newMVar (1, doneVar)
+  pure (n, chopVar)
 
---   (,)
---     <$> (Socket.new3 client =<< Network.getPeerName client)
---     <*> (Socket.new3 server =<< Network.getPeerName server)
-
--- mockServer :: IO Socket
--- mockServer = do
---   (client, server) <-
---     socketPair
-
---   parentThread <- myThreadId
-
---   void . forkIO . forever $
---     let
---       respond :: Proto.Message a => (a -> Response) -> IO ()
---       respond f =
---         Socket.sendResponse server (f Proto.defMessage)
---     in
---       Socket.receiveRequest server >>= \case
---         Nothing -> do
---           putStrLn "Riak disappeared?"
---           killThread parentThread
-
---         Just request ->
---           case request of
---             RequestDelete{}         -> respond ResponseDelete
---             RequestDeleteIndex{}    -> respond ResponseDelete
---             RequestGetBucket{}      -> respond ResponseGetBucket
---             RequestGetBucketType{}  -> respond ResponseGetBucket
---             RequestGetCrdt{}        -> respond ResponseGetCrdt
---             RequestGetIndex{}       -> respond ResponseGetIndex
---             RequestGetServerInfo{}  -> respond ResponseGetServerInfo
---             RequestGetSchema{}      -> respond ResponseGetSchema
---             RequestGet{}            -> respond ResponseGet
---             RequestListBuckets{}    -> respond ResponseListBuckets
---             RequestListKeys{}       -> respond ResponseListKeys
---             RequestMapReduce{}      -> respond ResponseMapReduce
---             RequestPing{}           -> respond ResponsePing
---             RequestPut{}            -> respond ResponsePut
---             RequestPutIndex{}       -> respond ResponsePut
---             RequestPutSchema{}      -> respond ResponsePut
---             RequestResetBucket{}    -> respond ResponseResetBucket
---             RequestSecondaryIndex{} -> respond ResponseSecondaryIndex
---             RequestSetBucket{}      -> respond ResponseSetBucket
---             RequestSetBucketType{}  -> respond ResponseSetBucket
---             RequestUpdateCrdt{}     -> respond ResponseUpdateCrdt
-
---   pure client
-
--- expectedResponse :: Request -> Response -> Bool
--- expectedResponse request response =
---   case request of
---     RequestDelete{} ->
---       case response of
---         ResponseDelete{} -> True
---         _ -> False
---     RequestDeleteIndex{} ->
---       case response of
---         ResponseDelete{} -> True
---         _ -> False
---     RequestGetBucket{} ->
---       case response of
---         ResponseGetBucket{} -> True
---         _ -> False
---     RequestGetBucketType{} ->
---       case response of
---         ResponseGetBucket{} -> True
---         _ -> False
---     RequestGetCrdt{} ->
---       case response of
---         ResponseGetCrdt{} -> True
---         _ -> False
---     RequestGetIndex{} ->
---       case response of
---         ResponseGetIndex{} -> True
---         _ -> False
---     RequestGetServerInfo{} ->
---       case response of
---         ResponseGetServerInfo{} -> True
---         _ -> False
---     RequestGetSchema{} ->
---       case response of
---         ResponseGetSchema{} -> True
---         _ -> False
---     RequestGet{} ->
---       case response of
---         ResponseGet{} -> True
---         _ -> False
---     RequestListBuckets{} ->
---       case response of
---         ResponseListBuckets{} -> True
---         _ -> False
---     RequestListKeys{} ->
---       case response of
---         ResponseListKeys{} -> True
---         _ -> False
---     RequestMapReduce{} ->
---       case response of
---         ResponseMapReduce{} -> True
---         _ -> False
---     RequestPing{} ->
---       case response of
---         ResponsePing{} -> True
---         _ -> False
---     RequestPut{} ->
---       case response of
---         ResponsePut{} -> True
---         _ -> False
---     RequestPutIndex{} ->
---       case response of
---         ResponsePut{} -> True
---         _ -> False
---     RequestPutSchema{} ->
---       case response of
---         ResponsePut{} -> True
---         _ -> False
---     RequestResetBucket{} ->
---       case response of
---         ResponseResetBucket{} -> True
---         _ -> False
---     RequestSecondaryIndex{} ->
---       case response of
---         ResponseSecondaryIndex{} -> True
---         _ -> False
---     RequestSetBucket{} ->
---       case response of
---         ResponseSetBucket{} -> True
---         _ -> False
---     RequestSetBucketType{} ->
---       case response of
---         ResponseSetBucket{} -> True
---         _ -> False
---     RequestUpdateCrdt{} ->
---       case response of
---         ResponseUpdateCrdt{} -> True
---         _ -> False
-
--- randomOneOf :: [a] -> IO a
--- randomOneOf xs =
---   (xs !!) <$> randomRIO (0, length xs - 1)
+chopCherryTree :: CherryTree -> IO ()
+chopCherryTree (n, chopVar) =
+  join $
+    modifyMVar chopVar $ \(m, doneVar) ->
+      if n == m
+        then do
+          pure ((m, doneVar), void (tryPutMVar doneVar ()))
+        else do
+          pure ((m+1, doneVar), readMVar doneVar)
