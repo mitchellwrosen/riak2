4d70455d2b90f13aea4241c516ac8344887ec9c6
Mitchell Rosen <mitchellwrosen@gmail.com>
2019-02-16 20:23:13 -0500

    benchmark more stuff

--- riak2-benchmarks/src/Main.hs
+++ riak2-benchmarks/src/Main.hs
@@ -5,19 +5,26 @@ import Riak.Handle.Impl.Exclusive (Endpoint(..))
 import qualified Riak.Handle.Impl.Exclusive        as Handle.Exclusive
 import qualified Riak.Handle.Impl.Pipeline         as Handle.Pipeline
 import qualified Riak.Handle.Impl.StripedExclusive as Handle.StripedExclusive
+import qualified Riak.Handle.Impl.StripedPipeline  as Handle.StripedPipeline
 import qualified Riak.WithExclusiveHandle
 import qualified Riak.WithPipelineHandle
 import qualified Riak.WithStripedExclusiveHandle
+import qualified Riak.WithStripedPipelineHandle
 
 import Control.Concurrent      (forkIO)
 import Control.Concurrent.MVar
+import Control.Lens
 import Control.Monad           (join, replicateM_)
 import Data.Default.Class      (Default(..))
 import GHC.Clock               (getMonotonicTime)
 import Net.IPv4                (ipv4)
 import Text.Printf             (printf)
 
-import qualified Options.Applicative as Opt
+import qualified Data.Riak.Proto              as Proto
+import qualified Network.Riak.Connection      as RHK
+import qualified Network.Riak.Connection.Pool as RHK
+import qualified Network.Riak.Types           as RHK
+import qualified Options.Applicative          as Opt
 
 
 main :: IO ()
@@ -33,7 +40,8 @@ commandParser :: Opt.Parser (IO ())
 commandParser =
   Opt.hsubparser
     (mconcat
-      [ Opt.command
+      [ Opt.commandGroup "riak2"
+      , Opt.command
           "exclusive-socket"
           (Opt.info
             exclusiveSocketParser
@@ -48,6 +56,18 @@ commandParser =
           (Opt.info
             stripedExclusiveSocketParser
             (Opt.progDesc "Benchmark striped exclusive socket handle"))
+      , Opt.command
+          "striped-pipeline-socket"
+          (Opt.info
+            stripedPipelineSocketParser
+            (Opt.progDesc "Benchmark striped pipeline socket handle"))
+
+      , Opt.commandGroup "riak-haskell-client"
+      , Opt.command
+          "riak-haskell-client"
+          (Opt.info
+            riakHaskellClientParser
+            (Opt.progDesc "Benchmark riak-haskell-client"))
       ])
 
 exclusiveSocketParser :: Opt.Parser (IO ())
@@ -136,6 +156,53 @@ stripedExclusiveSocketParser =
               pure ()))
 
     pure ()
+
+stripedPipelineSocketParser :: Opt.Parser (IO ())
+stripedPipelineSocketParser =
+  pure $ do
+    Right () <-
+      Handle.StripedPipeline.withHandle
+        (Handle.Pipeline.HandleConfig
+          { Handle.Pipeline.endpoint =
+              Endpoint
+                { address = ipv4 127 0 0 1
+                , port = 8087
+                }
+          , Handle.Pipeline.handlers =
+              mempty
+          })
+        (\handle ->
+          runBenchWith
+            (do
+              Right _ <-
+                Riak.WithStripedPipelineHandle.put
+                  handle
+                  (Riak.WithStripedPipelineHandle.newObject
+                    (Riak.WithStripedPipelineHandle.generatedKey
+                      (Riak.WithStripedPipelineHandle.Bucket "default" "a"))
+                    (Riak.WithStripedPipelineHandle.newContent ""))
+                  def
+              pure ()))
+
+    pure ()
+
+riakHaskellClientParser :: Opt.Parser (IO ())
+riakHaskellClientParser =
+  pure $ do
+    pool :: RHK.Pool <-
+      RHK.create
+        (RHK.Client "localhost" "8087" "")
+        1 -- stripes
+        10 -- seconds to keep idle connections open
+        10 -- conns per stripe
+
+    runBenchWith
+      (RHK.withConnection pool $ \conn -> do
+        _ <-
+          RHK.exchange
+            conn
+            (Proto.defMessage & Proto.bucket .~ "a" :: Proto.RpbPutReq)
+        pure ())
 
 runBenchWith :: IO () -> IO ()
 runBenchWith put = do
