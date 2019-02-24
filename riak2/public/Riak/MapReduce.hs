3387b6f77beecfc5772bc908116be94ae31e8521
Mitchell Rosen <mitchellwrosen@gmail.com>
2019-02-24 15:18:15 -0500

    Remove some dead code

--- riak2/public/Riak/MapReduce.hs
+++ riak2/public/Riak/MapReduce.hs
@@ -1,6 +1,7 @@
 -- |
 -- * <https://docs.basho.com/riak/kv/2.2.3/developing/usage/mapreduce/>
 -- * <https://docs.basho.com/riak/kv/2.2.3/developing/app-guide/advanced-mapreduce/>
+-- * <https://docs.basho.com/riak/kv/2.2.3/developing/api/protocol-buffers/mapreduce/>
 
 module Riak.MapReduce
   ( mapReduceBucket
